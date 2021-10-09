e21687841826ae1622f4c3b3a657595b555a03c4
dhlee3w <47702230+dhlee3w@users.noreply.github.com>
2021-10-09 16:07:54 -0500

    Update and rename App_ds.py to app_ds.py

--- App_ds.py
+++
@@ -1,4 +0,0 @@
-import streamlit as st
-
-st.write("## DS App")
-st.write("by D. Lee")
---
+++ app_ds.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+def app():
+  st.write("## DS App")
+  st.write("by D. Lee")
+
+if __name__=="__main__":
+  app()
